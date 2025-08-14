941066917aaf5630da2e6f1c157ae8c1f7878909
Mohammed <26S2053@utas.edu.om>
2025-08-14 12:57:00 +0400

    added the query method in Jpa job repositiry. also i added post method in the jobcontoller to create new job. still not tested

--- src/main/java/com/jobs/jobs/controllers/JobController.java
+++ src/main/java/com/jobs/jobs/controllers/JobController.java
@@ -1,8 +1,33 @@
 package com.jobs.jobs.controllers;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jobs.jobs.entities.Jobs;
+import com.jobs.jobs.repositories.JobRepository;
+
 @RestController
+@RequestMapping("api/jobs")
 public class JobController {
     
+@Autowired
+JobRepository jobRepo;
+
+@PostMapping
+public ResponseEntity<?> createJob( @RequestBody Jobs jobs){
+    boolean exists=jobRepo.findBySourceIdAndSourceJobId(jobs.getSourceId(),jobs.getSourceJobId()).isPresent();
+    if(exists) {return ResponseEntity.badRequest().body("Job with the same source_id and source_job_id already exists.");}
+    
+    Jobs saved=jobRepo.save(jobs);
+    return ResponseEntity.created(URI.create("/api/jobs/"+saved.getId())).body(saved);
+}
+
+
 }
--- src/main/java/com/jobs/jobs/repositories/JobRepository.java
+++ src/main/java/com/jobs/jobs/repositories/JobRepository.java
@@ -1,8 +1,16 @@
 package com.jobs.jobs.repositories;
 
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jobs.jobs.entities.Jobs;
 
 public interface JobRepository extends JpaRepository<Jobs,Integer>{
+    Page<Jobs> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String q1, String q2, Pageable p);
+    Page<Jobs> findByCompanyContainingIgnoreCase(String company, Pageable p);
+    Page<Jobs> findByLocationContainingIgnoreCase(String location, Pageable p);
+    Optional<Jobs> findBySourceIdAndSourceJobId(String sourceId, String sourceJobId);
 }
