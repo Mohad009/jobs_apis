68b5597eea50b93491e173bdfe71b7e45534c814
Mohammed <26S2053@utas.edu.om>
2025-08-14 13:41:18 +0400

    I added an api to create a job. i fixed an error where i face issue with storing  raw_payload

--- src/main/java/com/jobs/jobs/repositories/JobRepository.java
+++ src/main/java/com/jobs/jobs/repositories/JobRepository.java
@@ -2,8 +2,9 @@ package com.jobs.jobs.repositories;
 
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jobs.jobs.entities.Jobs;
