de0bd762c4aee6315701e2ffc96fd06d16fa6acd
Saud Al Maawali <soud111mawali@gmail.com>
2025-08-20 22:59:04 +0400

    Added search ~ NOT WORKING

--- src/main/java/com/jobs/jobs/controllers/JobController.java
+++ src/main/java/com/jobs/jobs/controllers/JobController.java
@@ -1,5 +1,6 @@
 package com.jobs.jobs.controllers;
 
+import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
@@ -45,5 +48,15 @@ public class JobController {
         return jobRepo.findAll(safe);
     }
 
-
+    @GetMapping("/search")
+    public ResponseEntity<List<Jobs>> searchJobs(
+            @RequestParam (required = false) String keyword,
+            @RequestParam (required = false) String location,
+            @RequestParam (required = false) BigDecimal salary_min,
+            @RequestParam (required = false) BigDecimal salary_max
+            //@RequestParam (required = false) String sentiment
+    ) {
+        List<Jobs> jobs = jobRepo.searchJobs(keyword, location, salary_min, salary_max);
+        return new ResponseEntity<>(jobs, HttpStatus.OK);
+    }
 }
--- src/main/java/com/jobs/jobs/repositories/JobRepository.java
+++ src/main/java/com/jobs/jobs/repositories/JobRepository.java
@@ -1,13 +1,28 @@
 package com.jobs.jobs.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jobs.jobs.entities.Jobs;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JobRepository extends JpaRepository<Jobs,Integer>{
 
+    @Query("SELECT j from Jobs j WHERE " +
+            "LOWER(j.location) LIKE LOWER(CONCAT('%', :location, '%')) OR " +
+            "LOWER(j.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+            //"j.salaryMin = ?salary_min OR " +
+            //"LOWER(j.salaryMax) LIKE LOWER(CONCAT('%', :salary_max, '%'))")
+            // "LOWER(j.sentiment) LIKE LOWER(CONCAT('%', :sentiment, '%'))")
+    public List<Jobs> searchJobs(String location, String keyword, BigDecimal salary_min, BigDecimal salary_max);
+    
     Optional<Jobs> findBySourceIdAndSourceJobId(String sourceId, String sourceJobId);
+
+    Optional<Jobs> getDistinctByDescription(String description);
+
+    List<Jobs> queryDistinctByLocationContains(String location);
 }
